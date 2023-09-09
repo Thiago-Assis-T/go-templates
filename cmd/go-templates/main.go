d806c2a30cc47182c8a680f48143a6dcb60aa120
Thiago-Assis-T <thiago.assisfernandes@gmail.com>
2023-09-09 13:57:08 -0300

    stuff?

--- cmd/go-templates/main.go
+++ cmd/go-templates/main.go
@@ -1,4 +1,36 @@
 package main
 
+import (
+	"html/template"
+	"net/http"
+)
+
+var tmpl *template.Template
+
+type Todo struct {
+	Item string
+	Done bool
+}
+
+type PageData struct {
+	Title string
+	Todos []Todo
+}
+
+func todo(w http.ResponseWriter, r *http.Request) {
+	data := PageData{
+		Title: "TODO List",
+		Todos: []Todo{
+			{Item: "Install Go", Done: true},
+			{Item: "Learn Go", Done: false},
+			{Item: "Make an API with Go", Done: false},
+			{Item: "Make a Site with Go Templates", Done: false},
+		},
+	}
+}
+
 func main() {
+	mux := http.NewServeMux()
+	tmpl := template.Must(template.ParseFiles("templates/index.html"))
+	mux.HandleFunc("/todo", todo)
 }
